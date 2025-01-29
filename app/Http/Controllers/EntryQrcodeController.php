79ab05389814cb60d59cbd90d55eb54ecf58cae0
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2025-01-29 16:16:58 +0900

    test commit 16

--- app/Http/Controllers/EntryQrcodeController.php
+++ app/Http/Controllers/EntryQrcodeController.php
@@ -23,7 +23,7 @@ class EntryQrcodeController extends Controller
     }
 
     public function store(Request $request) {
-        $entry = Entry::where('event_id', $request->data->event->id)->where('user_id', $request->user_id)->first();
+        $entry = Entry::where('event_id', $request->event_id)->where('user_id', $request->user_id)->first();
         $entry->is_entry = true;
         $entry->save();
         return redirect()->route('admission.already', ['event_id' => $request->event_id]);
