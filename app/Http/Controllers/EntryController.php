e773968741aed07d927dfc6ed6d9b5884867d7f4
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2025-01-29 15:59:10 +0900

    test commit 8

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -10,8 +10,8 @@ class EntryController extends Controller
 {
     public function show() {
         $applied = EntryView::where('user_id', auth()->id())->pluck('event_id')->toArray();
-        $applied_events = Event::whereIn('id', $applied)->get();
-        $not_applied_events = Event::whereNotIn('id', $applied)->get();
+        $applied_events = Event::whereIn('id', $applied)->orderBy('date')->get();
+        $not_applied_events = Event::whereNotIn('id', $applied)->orderBy('date')->get();
         return view('entry.show', compact('applied_events', 'not_applied_events'));
     }
 }
